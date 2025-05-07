42aed3751c3394ea3c69d66529d266e7d21bb15a
Clarete-ana <anaclaraclarete2@gmail.com>
2025-05-07 15:33:59 -0300

    First commit

---
+++ src/main/java/me/dio/projetoApiRest/ProjetoApiRestApplication.java
@@ -0,0 +1,13 @@
+package me.dio.projetoApiRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoApiRestApplication.class, args);
+	}
+
+}
